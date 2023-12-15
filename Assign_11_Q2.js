ac0bc8ce5ffc15e1f31c33bb54204e2c4e374c3e
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2023-12-15 12:38:04 -0600

    Final Commit

--- Assign_11_Q2.js
+++ Assign_11_Q2.js
@@ -1,35 +1,61 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+// let http = require("http");
+// let fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const { url } = req;
 
-    if (url === '/image') {
-        // Send image
+// const server = http.createServer((request, response) => {
+//     let path = request.url;
+
+//     if (path === '/home' || path.toLowerCase() === '/home')
+//         response.end("Welcome to my website");
+//     else if (path.toLowerCase() === '/image')
+//         response.end("You are in about page");
+//     else if (path.toLowerCase() === '/pdf')
+//         response.end("You are in contact page");
+//     else {
+//         response.writeHead(404);
+//         response.end("Error 404: Page not found");
+//     }
+// })
+
+// server.listen(3000, '127.0.0.1', () => {
+//     console.log('Server has started...');
+// });
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((request, response) => {
+    let url = request.url.toLowerCase();
+
+    if (url === '/home') {
+        response.end("Welcome to my website");
+    } else if (url === '/image') {
+        // Read the image file
         const imagePath = path.join(__dirname, 'images', 'maxresdefault.jpg');
-        const imageStream = fs.createReadStream(imagePath);
-        res.setHeader('Content-Type', 'image/jpg');
-        imageStream.pipe(res);
+        const imageData = fs.readFileSync(imagePath);
+
+        // Set the appropriate content type
+        response.setHeader('Content-Type', 'image/jpeg');
+
+        // Send the image data as the response
+        response.end(imageData);
     } else if (url === '/pdf') {
-        // Send PDF
+        // Read the PDF file
         const pdfPath = path.join(__dirname, 'lesson10-Event handling.pdf');
-        const pdfStream = fs.createReadStream(pdfPath);
-        res.setHeader('Content-Type', 'application/pdf');
-        pdfStream.pipe(res);
-    } else if (url === '/home') {
-        // Send text
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Welcome to my website');
+        const pdfData = fs.readFileSync(pdfPath);
+
+        // Set the appropriate content type
+        response.setHeader('Content-Type', 'application/pdf');
+
+        // Send the PDF data as the response
+        response.end(pdfData);
     } else {
-        // Handle other paths or send a 404 response
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
+        response.writeHead(404);
+        response.end("Error 404: Page not found");
     }
 });
 
-const PORT = 8888;
-
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server has started...');
 });
