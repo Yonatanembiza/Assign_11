d7f3b9413dd3351d24ca33d2bd3c0b08cc2e993b
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2023-12-14 22:01:40 -0600

    commited

---
+++ dom.js
@@ -0,0 +1,8 @@
+var document = window.document;
+console.log(document)
+var myNAvigator = window.navigator;
+console.log(myNAvigator)
+var myVariable = "Hello";
+console.log(myVariable)
+document.write("Hello there!")
+document.createElement("P")
